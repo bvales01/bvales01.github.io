d7ba5dde3c3e613e703121c6000d8d464b013d4f
Brianna <briannavales7@gmail.com>
2026-02-28 14:34:01 -0500

    Added the JavaScript file

---
+++ script.js
@@ -0,0 +1,28 @@
+async function loadGitHubRepos() {
+    const container = document.getElementById('repo-container');
+
+    try {
+        const repsonse = await fetch('https://github.com/bvales01?tab=repositories');
+        const data = await response.json();
+
+        container.innerHTML = '';
+
+        data.forEach(repo => {
+            const card = document.createElement('div');
+            card.className = 'project-card';
+
+            card.innerHTML = `
+            <h3>${repo.name}</h3>
+            <p>${repo.description || "No description provided."}</p>
+            <div class="repo-stats">
+            </div>
+            <a href="${repo.html_url}" target="_blank" class="btn">View Source</a>
+            `;
+            container.appendChild(card);
+        });
+    } catch (error) {
+        console.error('Error fetching repos:', error);
+        document.getElementById('repo-container').innerHTML = "<p>Failed to load projects.</p>";
+    }
+};
+
